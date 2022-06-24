7e0d2fa2c40dfe8f4044d1c34273f9a38fd1375e
HarshDevelops <76532840+HarshDevelops@users.noreply.github.com>
2022-06-24 13:57:03 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,78 @@
+from email.policy import default
+from flask import Flask, render_template, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+app=Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///swiftskuharsh.db"
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS']=False
+db=SQLAlchemy(app)
+
+class contactsearch(db.Model):
+    person_sno = db.Column(db.Integer, primary_key=True)
+    person_name=db.Column(db.String(50), nullable=False)
+    person_second_name=db.Column(db.String(100))
+    person_number=db.Column(db.Integer) 
+    person_alt_number=db.Column(db.Integer) 
+
+    def __repr__(self) -> str:
+        return f"{self.person_name} - {self.person_second_name}"
+
+
+
+@app.route('/' ,methods=['GET', 'POST'])
+def indexx():
+    if(request.method=='POST'):
+        first_name = request.form['first_name']
+        phone = ((request.form['phone']).replace(" ", ""))
+        if((contactsearch.query.filter_by(person_number=phone).first())!=None):
+            flash('A contact with this phone number already exists')
+            
+        else:
+            phone=int(phone)
+        last_name= request.form['last_name']
+        altphone= (request.form['altphone'])
+        if(len(altphone)==0):
+            altphone=0
+        else:
+            altphone=(altphone.replace(" ", ""))
+            altphone=int(altphone)
+        contact=contactsearch(person_name=first_name, person_second_name=last_name,person_number=phone, person_alt_number=altphone)
+        db.session.add(contact)
+        db.session.commit()
+    allcontact= contactsearch.query.all()
+    return render_template('index.html', allcontact=allcontact)
+
+
+@app.route('/contacts')
+def index():
+    allcontact= contactsearch.query.all()
+    print(allcontact)
+    return "THIS IS CONTACTS PAGE"
+
+
+@app.route('/delete/<int:person_sno>/<int:person_number>')
+def delete(person_sno,person_number):
+    allcontact= contactsearch.query.filter_by(person_sno=person_sno,person_number=person_number).first()
+    db.session.delete(allcontact)
+    db.session.commit()
+    return redirect('/')
+
+
+@app.route('/update')
+def update():
+    allcontact= contactsearch.query.all()
+    print(allcontact)
+    return "THIS IS CONTACTS PAGE"
+
+
+@app.route('/new')
+def fulldetails():
+    allcontact= contactsearch.query.all()
+    print(allcontact)
+    return render_template('result.html')
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
