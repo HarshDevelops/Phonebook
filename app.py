7d96744f9a060074bc27e89d536859f86882981b
HarshDevelops <76532840+HarshDevelops@users.noreply.github.com>
2022-06-28 00:05:28 +0530

    Debugging

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class contactsearch(db.Model):
     person_name=db.Column(db.String(50), nullable=False)
     person_second_name=db.Column(db.String(100))
     person_number=db.Column(db.Integer) 
-    person_alt_number=db.Column(db.Integer) 
+    person_email=db.Column(db.String(50), nullable=True)
 
     def __repr__(self) -> str:
         return f"{self.person_name} - {self.person_second_name}"
@@ -31,38 +31,17 @@ def indexx():
         else:
             phone=int(phone)
         last_name= request.form['last_name']
-        altphone= (request.form['altphone'])
-        if(len(altphone)==0):
-            altphone=0
-        else:
-            altphone=(altphone.replace(" ", ""))
-            altphone=int(altphone)
-        contact=contactsearch(person_name=first_name, person_second_name=last_name,person_number=phone, person_alt_number=altphone)
+        email= (request.form['email'])
+        # if(len(altphone)==0):
+        #     altphone=0
+        # else:
+        #     altphone=(altphone.replace(" ", ""))
+        #     altphone=int(altphone)
+        contact=contactsearch(person_name=first_name, person_second_name=last_name,person_number=phone, person_email=email)
         db.session.add(contact)
         db.session.commit()
     allcontact= contactsearch.query.all()
     return render_template('index.html', allcontact=allcontact)
-
-
-@app.route('/search/elo',methods=['GET', 'POST'])
-def index():
-    if(request.method=='POST'):
-         print("INSIDE POST")
-         to_be_found = request.form['contact_search']
-         print("tbf: ", to_be_found)
-         flag=0
-         if(contactsearch.query.filter_by(person_name=to_be_found).all()!=0):
-            flag=1
-         elif(contactsearch.query.filter_by(person_second_name=to_be_found).all()!=0):
-            flag=2
-         else:
-            flag=3
-
-         if(flag==3):
-            allcontact=contactsearch.query.filter_by(person_name=to_be_found).first()
-    # allcontact= contactsearch.query.filter_by(person_second_name=to_be_found).all()
-    
-    return redirect('/')
 
 
 
@@ -81,25 +60,22 @@ def update(person_sno,person_number):
         phone = ((request.form['phone']).replace(" ", ""))
         phone=int(phone)
         last_name= request.form['last_name']
-        altphone= (request.form['altphone'])
-        print(altphone)
-        print(len(altphone))
-        if(len(altphone)==0 or altphone==" "):
-            print("got it")
-            altphone=0
+        email= (request.form['email'])
+        if(len(email)==0):
+            print("inseide")
+            email=" "
         else:
-            print("got else")
-            altphone=(altphone.replace(" ", ""))
-            altphone=int(altphone)
-
-        contact=contactsearch(person_name=first_name, person_second_name=last_name,person_number=phone, person_alt_number=int(altphone))
+            email=email
+        
+        print(email)
+        contact=contactsearch(person_name=first_name, person_second_name=last_name,person_number=phone, person_email=email)
         
         allcontact= contactsearch.query.filter_by(person_sno=person_sno,person_number=person_number).first()
         
         allcontact.person_name=first_name
         allcontact.person_second_name=last_name
         allcontact.person_number=phone
-        allcontact.person_alt_number=int(altphone)
+        allcontact.person_dmail=email
         db.session.add(allcontact)
         db.session.commit()
         flash("Contact Successfully Updated!")
